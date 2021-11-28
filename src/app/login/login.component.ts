46f5ce8530405231cb145238be596fd20b49eb25
sofiaBouaklayene <sofia.bouaklayene@gmail.com>
2021-11-28 21:49:33 +0100

    my modification

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
