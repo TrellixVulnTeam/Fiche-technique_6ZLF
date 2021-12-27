3eac976f509c1b048b240ac2f418981ad84c4caa
nourela <nourounaelhouda@gmail.com>
2021-12-27 21:43:47 +0100

    resol probleme formulaire fich

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase :{
-    apiKey: "AIzaSyB50FwwBtEjteoRKDDSFPLDqKieOzdwxQc",
-    authDomain: "awi-projet.firebaseapp.com",
-    projectId: "awi-projet",
-    storageBucket: "awi-projet.appspot.com",
-    messagingSenderId: "337076673291",
-    appId: "1:337076673291:web:e8082f9b9ed2ac3d89a00e"
+    apiKey: "AIzaSyAk6j8kMDxibZroeTsdOFAPtTID18V2bV8",
+    authDomain: "fiche-awi.firebaseapp.com",
+    databaseURL: "https://fiche-awi-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "fiche-awi",
+    storageBucket: "fiche-awi.appspot.com",
+    messagingSenderId: "320283919140",
+    appId: "1:320283919140:web:56267270fd89e44bec69b1"
 
   }
 };
