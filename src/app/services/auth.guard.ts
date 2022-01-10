0bb94f3535806c6ee4617bdbe69767ce0c6658d5
nourela <nourounaelhouda@gmail.com>
2022-01-10 15:28:42 +0100

    Accés hors connexion

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private afAuth: AngularFireAuth) {
 
   }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    return new Promise(
+      (resolve,reject)=>{
+        this.afAuth.onAuthStateChanged(
+          (user)=>{
+            if(user){
+              resolve(true);
+            }else{
+              this.router.navigate(['/login']);
+              resolve(false);
+            }
+          }
+        );
+      }
+    );
   }
   
 }
